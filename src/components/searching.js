07dd993a1daefd72a5895645cb4f96dadcb3bc24
Evgeny Razorenov <Dumpil5@gmail.com>
2025-09-19 14:28:12 +0300

    Первая попытка

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,19 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison([
+        rules.skipEmptyTargetValues,
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
+    ]);
+
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => {
+            const value = state.search?.toLowerCase();
+            return ['date', 'customer', 'seller'].some(field => {
+                return row[field]?.toLowerCase().includes(value);
+            });
+        });
     }
 }
